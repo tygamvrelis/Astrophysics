6b376911f787b71175b58b934f496c6751ac37e2
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2019-04-25 23:35:29 -0400

    Update old plots

--- src/definitions.h
+++ src/definitions.h
@@ -8,7 +8,7 @@
 #define  TIME_STEPPING           RK2
 #define  DIMENSIONAL_SPLITTING   NO
 #define  NTRACER                 0
-#define  USER_DEF_PARAMETERS     4
+#define  USER_DEF_PARAMETERS     5
 
 /* -- physics dependent declarations -- */
 
@@ -27,21 +27,38 @@
 #define  VMAX                    1
 #define  EXP_DECAY               2
 #define  TRELAX                  3
+#define  BSURFACE                4
 
 /* [Beg] user-defined constants (do not change this line) */
 
+// ----------------------------------------------------------------------------
+// Winds: Exponential or quadratic profile, or none
+// ----------------------------------------------------------------------------
+#define  WIND_EXP
+// #define WIND_QUAD
+
+// ----------------------------------------------------------------------------
+// Magnetic field: uniform vertical field, dipole, or none
+// ----------------------------------------------------------------------------
+#define B_DIPOLE
+// #define B_UNIFORM
+
+// ----------------------------------------------------------------------------
+// Planet parameters
+// ----------------------------------------------------------------------------
 #define  CONST_Rjupiter          6.9911e9
 #define  CONST_Rplanet           (1.6*CONST_Rjupiter)
 #define  CONST_period            (3.0*24.0*3600.0)
-#define  WIND_EXP                //
 
+// ----------------------------------------------------------------------------
+// Scaling
+// ----------------------------------------------------------------------------
 // With unit length defined this way, CONST_Rplanet will measure the radius of
 // the planet all the way up to the end of the R-coordinate, as desired. At the
 // same time, R=1 will not be the edge of the planet (R=g_domEnd[IDIR] will be)
 // but will instead define the location where the gravity body force changes,
 // as before
 #define UNIT_LENGTH         (CONST_Rplanet/g_domEnd[IDIR])
-
 #define UNIT_DENSITY        1e-9
 
 /* [End] user-defined constants (do not change this line) */
--- src/init.c
+++ src/init.c
@@ -60,16 +60,46 @@ void Init (double *us, double x1, double x2, double x3)
 
 //-----------------------------------------------------------------------------
 
+#if defined(B_UNIFORM) || defined (B_DIPOLE)
 // Sets initial curl-free magnetic field component
 // Runs after init.
+static bool first = true;
+static double M;
 void BackgroundField (double x1, double x2, double x3, double *B0)
-{  
-  // Magnetic pressure: Bz << sqrt(8*pi*P)
-  double Bz = sqrt(8.0 * CONST_PI * min_P) / 1000.0;
-  B0[0] = cos(x2) * Bz;      // r
-  B0[1] = -1 * sin(x2) * Bz; // theta
-  B0[2] = 0;                 // phi
+{
+  #if defined(UNIFORM)
+  {
+    // Magnetic pressure: Bz << sqrt(8*pi*P)
+    double Bz = sqrt(8.0 * CONST_PI * min_P) / 1000.0;
+    B0[0] = cos(x2) * Bz;      // r
+    B0[1] = -1 * sin(x2) * Bz; // theta
+    B0[2] = 0;                 // phi
+  }
+  #elif defined(B_DIPOLE)
+  {
+    if (first)
+    {
+      first = false;
+      // Convert input to code units
+      double B = g_inputParam[BSURFACE] / (
+        UNIT_VELOCITY * sqrt(4 * CONST_PI * UNIT_DENSITY)
+      );
+
+      // |M| = (B * r^3) / (1 + 3*(cos(theta))^2)
+      // At equator (theta = 90), this reduces to |M| = B * r^3
+      // TODO: What r-value is the "surface value" supposed to be taken at?
+      // For now assume it's the edge of the planet
+      M = B * pow(g_domEnd[IDIR], 3);
+    }
+
+    double r_cubed = pow(x1, 3);
+    B0[0] = 2 * M * cos(x2) / r_cubed;
+    B0[1] = M * sin(x2) / r_cubed;
+    B0[2] = 0.0;
+  }
+  #endif
 }
+#endif
 
 //-----------------------------------------------------------------------------
 
