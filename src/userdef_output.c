93f64f51a38f3cb0f65953d002ac31e324261d93
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2019-05-09 20:02:46 -0400

    work in progress - attempt to dump current vectors as user vars

---
+++ src/userdef_output.c
@@ -0,0 +1,25 @@
+#include "pluto.h"
+
+/**
+ * @brief Dumps the current vectors
+ */
+void ComputeUserVar (const Data *d, Grid *grid)
+{
+  int i, j, k;
+  double ***Jx1 = GetUserVar("Jx1");
+  double ***Jx2 = GetUserVar("Jx2");
+  double ***Jx3 = GetUserVar("Jx3");
+  
+  DOM_LOOP(k,j,i){
+  	Jx1[k][j][i] = d->J[IDIR][k][j][i];
+  	Jx2[k][j][i] = d->J[JDIR][k][j][i];
+  	Jx3[k][j][i] = d->J[KDIR][k][j][i];
+  }
+}
+
+//-----------------------------------------------------------------------------
+
+void ChangeOutputVar ()
+{ 
+
+}
